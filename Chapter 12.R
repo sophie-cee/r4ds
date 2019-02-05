409d2ea240a09dd09ef68a2114a9a2d9d4697b0c
Sophie Christensen <sophia.e.christensen@gmail.com>
2019-02-05 11:46:46 -0700

    Today is tuesday

--- Chapter 12.R
+++ Chapter 12.R
@@ -62,3 +62,14 @@ newtable2 <- table2 %>%
   spread(key = type, value = count)
 newtable2
 
+library(tidyverse)
+stocks <- tibble(
+  year   = c(2015, 2015, 2016, 2016),
+  half  = c(   1,    2,     1,    2),
+  return = c(1.88, 0.59, 0.92, 0.17)
+)
+stocks %>% 
+  spread(year, return) %>% 
+  gather("year", "return", `2015`:`2016`)
+
+
